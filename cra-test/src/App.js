4f18ed14cda06f9f359cfd1fcacf362956de13ea
gudwlsa <gudwlsa@gmail.com>
2020-02-13 10:29:08 +0900

    Test

---
+++ cra-test/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./TodoList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoList />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome to React</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
--- simple2.js
+++ simple2.js
@@ -1,26 +1,26 @@
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false};
+    this.state = { liked: false };
   }
   render() {
-    const text = this.state.liked ? '좋아요 취소' : '좋아요';
+    const text = this.state.liked ? "좋아요 취소" : "좋아요";
     return React.createElement(
-      'button',
-      { onClick: () => this.setState({ liked: true})},
-      text,
+      "button",
+      { onClick: () => this.setState({ liked: true }) },
+      text
     );
   }
 }
 ReactDOM.render(
   React.createElement(LikeButton),
-  document.querySelector('#react-root1'),
+  document.querySelector("#react-root1")
 );
 ReactDOM.render(
   React.createElement(LikeButton),
-  document.querySelector('#react-root2'),
+  document.querySelector("#react-root2")
 );
 ReactDOM.render(
   React.createElement(LikeButton),
-  document.querySelector('#react-root3'),
+  document.querySelector("#react-root3")
 );
